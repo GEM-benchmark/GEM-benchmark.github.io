2e81f0fe6ac5df4d8a605a2e727dc0ef89f660fa
Sebastian Gehrmann <s.gehrmann@outlook.com>
2020-10-31 16:13:43 -0400

    v0 of getting started page with markdown

--- web/pages/get_started.js
+++ web/pages/get_started.js
@@ -1,3 +1,30 @@
-export default function FirstPost() {
-  return <h1>Getting Started <code> coding</code>.</h1>
+import { getAllTaskIds, getTaskData } from "../../lib/tasks";
+import Layout from "../../components/layout";
+import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ tutorialData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>GEM Getting Started</title>
+      </Head>
+      <article>
+        <span className={utilStyles.headingXl}>
+          Getting Started <code> coding</code>.
+        </span>
+        <span className={utilStyles.smallSpace}></span>
+        <div dangerouslySetInnerHTML={{ __html: tutorialData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const taskData = await getTutorialData();
+  return {
+    props: {
+      tutorialData,
+    },
+  };
 }
