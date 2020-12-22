2943d0f7a5457712473c08b63e33def2379fe035
Sebastian Gehrmann <gehrmann@google.com>
2020-12-22 17:39:14 -0500

    Deploy gh-pages

--- web/out/_next/static/development/_buildManifest.js
+++ web/out/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/tasks/guide":["static\u002Fchunks\u002Fpages\u002Ftasks\u002Fguide.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftasks\u002Fguide"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/team":["static\u002Fchunks\u002Fpages\u002Fteam.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fteam"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
