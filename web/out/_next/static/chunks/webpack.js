de15a62fbd05c50251049fde97e83ecc37b37cc7
Sebastian Gehrmann <gehrmann@google.com>
2021-10-26 17:36:57 -0400

    Deploy gh-pages

--- web/out/_next/static/chunks/webpack.js
+++ web/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c5b9fc61220f954c6f1d";
+/******/ 	var hotCurrentHash = "0221e34633ea3f729cf6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- web/out/_next/static/zGOGK_crf6PpLJdyGAiGi/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fteam\u002F2021","\u002Fteam\u002F2022","\u002Fresults","\u002Fdata_cards","\u002Fshared_task","\u002Fturker_faq","\u002Fdata_cards\u002Fguide","\u002Fmodel_cards","\u002Fget_started","\u002Fworkshop\u002F2021","\u002Fnl_augmenter","\u002Fdata_cards\u002F[id]","\u002Fmodel_cards\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
