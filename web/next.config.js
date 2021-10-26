94d4dd3491cc99faadfa5e82734e4f5065ca86a1
Sebastian Gehrmann <gehrmann@google.com>
2021-10-26 13:10:02 -0400

    move panel redirect to next config from page.

--- web/next.config.js
+++ web/next.config.js
@@ -6,6 +6,12 @@ module.exports = {
           destination: '/workshop/2021',
           permanent: true,
         },
+        {
+          source: '/panel',
+          destination: 'https://www.dory.app/c/google.com/a89b88b9_gem-workshop',
+          permanent: true,
+
+        }
       ]
     },
   }
