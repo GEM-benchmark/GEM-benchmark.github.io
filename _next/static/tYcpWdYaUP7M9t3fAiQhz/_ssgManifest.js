77b33aab0a37ebcc264071be2936d3aaa27ade9d
Sebastian Gehrmann <gehrmann@google.com>
2022-03-29 14:05:42 -0400

    Deploy gh-pages

--- _next/static/tYcpWdYaUP7M9t3fAiQhz/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fteam\u002F2021","\u002Fteam","\u002Fresults","\u002Fdata_cards","\u002Ftutorials","\u002Fturker_faq","\u002Fmodel_cards","\u002Fshared_task","\u002Fnl_augmenter","\u002Fhackathon","\u002Fworkshop","\u002Fdata_cards\u002F[id]","\u002Ftutorials\u002F[id]","\u002Fmodel_cards\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
